34d701efdea484ff02cd0f2e81b6927b594dffc9
bhargav08-code <85948072+bhargav08-code@users.noreply.github.com>
2024-06-09 02:52:47 +0530

    Update page.tsx

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -12,8 +12,8 @@ function page() {
       </h1>
       <div className="flex flex-wrap justify-center">
         {featureData.features.map((course) => (
-          <CardContainer className="inter-var m-4">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardContainer key={course.id} className="inter-var m-4">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
               <CardItem
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
